4317c886ea3ea2669b0dd884c3775b2a23b09f52
come25136 <come25136@gmail.com>
2016-05-17 20:50:39 +0900

    add command mode

--- src/Main.java
+++ src/Main.java
@@ -13,14 +13,15 @@ import java.awt.event.*;
 
 public class Main implements NativeKeyListener {
     private Robot robot = new Robot();
-    int ent_flag = 0;
-    int t_flag = 0;
+    int ent_flag = 0; // Enter key flag
+    int t_flag = 0; // t key flag
+    int slash_flag = 0; // slash key flag
 
     public Main() throws AWTException {
     }
 
     public static void main(String[] args) {
-        // アイコンイメージの読み込み
+        // icon file read
         Image image = null;
         try {
             image = ImageIO.read(Thread.currentThread()
@@ -30,13 +31,13 @@ public class Main implements NativeKeyListener {
             e.printStackTrace();
         }
 
-        // トレイアイコン生成
+        // tray icon creating
         TrayIcon icon = new TrayIcon(image);
 
-        // ポップアップメニュー
+        // pop-up menu
         PopupMenu menu = new PopupMenu();
 
-        // 終了メニュー
+        // exit menu
         MenuItem exitItem = new MenuItem("終了");
         exitItem.addActionListener(e -> System.exit(0));
         menu.add(exitItem);
@@ -67,33 +68,37 @@ public class Main implements NativeKeyListener {
     }
 
     public void nativeKeyPressed(NativeKeyEvent e) {
-        if (e.getKeyCode() == NativeKeyEvent.VC_T) {
-            if (t_flag == 0) {
-                robot.keyPress(KeyEvent.VK_HALF_WIDTH);
-                robot.keyRelease(KeyEvent.VK_HALF_WIDTH);
+        if (e.getKeyCode() == NativeKeyEvent.VC_T) { // t key
+            if (slash_flag == 0 && t_flag == 0) {
+                robot.keyPress(KeyEvent.VK_HALF_WIDTH); // Press the half-size / em keys
+                robot.keyRelease(KeyEvent.VK_HALF_WIDTH); // Releasing the half-size / em keys
                 t_flag = 1;
             }
         }
 
-        if (e.getKeyCode() == NativeKeyEvent.VC_ENTER) {
-            if (t_flag == 1) {
-                if (ent_flag == 1) {
-                    Robot robot = null;
-                    try {
-                        robot = new Robot();
-                    } catch (AWTException e1) {
-                        e1.printStackTrace();
-                    }
-                    robot.keyPress(KeyEvent.VK_HALF_WIDTH);
-                    robot.keyRelease(KeyEvent.VK_HALF_WIDTH);
-                    t_flag = 0;
-                    ent_flag = 0;
-                } else {
-                    ent_flag = 1;
+        if (e.getKeyCode() == NativeKeyEvent.VC_ENTER) { // Enter key
+            if (t_flag == 1 && ent_flag == 1) {
+                Robot robot = null;
+                try {
+                    robot = new Robot();
+                } catch (AWTException e1) {
+                    e1.printStackTrace();
                 }
+                robot.keyPress(KeyEvent.VK_HALF_WIDTH); // Press the half-size / em keys
+                robot.keyRelease(KeyEvent.VK_HALF_WIDTH); // Releasing the half-size / em keys
+                t_flag = 0;
+                ent_flag = 0;
+            } else if (slash_flag == 1) {
+                slash_flag = 0;
+            } else {
+                ent_flag = 1;
             }
         } else {
             ent_flag = 0;
+        }
+
+        if (e.getKeyCode() == NativeKeyEvent.VC_SLASH) { // slash key
+            slash_flag = 1;
         }
     }
 
