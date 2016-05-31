bef3cc1f96c5c98a8a587289907e517dc74252f2
syuchan1005@syu_chan_1005 <syuchan.dev@gmail.com>
2016-05-31 23:35:53 +0900

    PullReq用

--- src/Main.java
+++ src/Main.java
@@ -1,110 +1,87 @@
 import org.jnativehook.GlobalScreen;
 import org.jnativehook.NativeHookException;
 import org.jnativehook.keyboard.NativeKeyEvent;
-import org.jnativehook.keyboard.NativeKeyListener;
 
-import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import java.awt.Image;
+import java.awt.MenuItem;
+import java.awt.PopupMenu;
+import java.awt.Robot;
+import java.awt.SystemTray;
+import java.awt.TrayIcon;
+import java.awt.event.KeyEvent;
 import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import java.awt.*;
-import java.awt.event.*;
 
-public class Main implements NativeKeyListener {
-    private Robot robot = new Robot();
-    int ent_flag = 0; // Enter key flag
-    int t_flag = 0; // t key flag
-    int slash_flag = 0; // slash key flag
+public class Main extends NativeKeyAdapter {
+	private Robot robot;
+	private int ent_flag = 0;
+	private int t_flag = 0;
+	private int slash_flag = 0;
 
-    public Main() throws AWTException {
-    }
+	public Main() throws Exception {
+		robot = new Robot();
+		Logger logger = Logger.getLogger("GlobalScreen");
+		logger.setLevel(Level.WARNING);
+		SystemTray.getSystemTray().add(createMenu());
 
-    public static void main(String[] args) {
-        // icon file read
-        Image image = null;
-        try {
-            image = ImageIO.read(Thread.currentThread()
-                    .getContextClassLoader()
-                    .getResourceAsStream("icon/icon.png"));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+		if (!GlobalScreen.isNativeHookRegistered()) {
+			try {
+				GlobalScreen.registerNativeHook();
+			} catch (NativeHookException e) {
+				e.printStackTrace();
+				System.exit(1);
+			}
+		}
+		GlobalScreen.addNativeKeyListener(this);
+	}
 
-        // tray icon creating
-        TrayIcon icon = new TrayIcon(image);
+	public static Image roadImageInJar(String path) throws IOException {
+		return new ImageIcon(ClassLoader.getSystemClassLoader().getResource(path)).getImage();
+	}
 
-        // pop-up menu
-        PopupMenu menu = new PopupMenu();
+	public static TrayIcon createMenu() throws IOException {
+		PopupMenu menu = new PopupMenu();
+		MenuItem exitItem = new MenuItem("終了");
+		exitItem.addActionListener(e -> System.exit(0));
+		menu.add(exitItem);
+		return new TrayIcon(roadImageInJar("icon/icon.png"), "MJI", menu);
+	}
 
-        // exit menu
-        MenuItem exitItem = new MenuItem("終了");
-        exitItem.addActionListener(e -> System.exit(0));
-        menu.add(exitItem);
-        icon.setPopupMenu(menu);
-        try {
-            SystemTray.getSystemTray().add(icon);
-        } catch (AWTException e) {
-            e.printStackTrace();
-        }
+	@Override
+	public void nativeKeyPressed(NativeKeyEvent e) {
+		if(e.getKeyCode() == NativeKeyEvent.VC_ENTER) {
+			if(t_flag == 1 && ent_flag == 1) {
+				this.pushHankaku();
+				t_flag = 1;
+				ent_flag = 0;
+			} else if (slash_flag == 1) {
+				slash_flag = 0;
+			} else {
+				ent_flag = 1;
+			}
+			return;
+		} else if(e.getKeyCode() == NativeKeyEvent.VC_T) {
+			this.pushHankaku();
+			t_flag = 1;
+		} else if (e.getKeyCode() == NativeKeyEvent.VC_SLASH){
+			slash_flag = 1;
+		}
+		ent_flag = 0;
+	}
 
-        if (!GlobalScreen.isNativeHookRegistered()) {
-            try {
-                GlobalScreen.registerNativeHook();
-            } catch (NativeHookException e) {
-                e.printStackTrace();
-                System.exit(-1);
-            }
-        }
+	public void pushHankaku() {
+		robot.keyPress(KeyEvent.VK_HALF_WIDTH);
+		robot.keyRelease(KeyEvent.VK_HALF_WIDTH);
+	}
 
-        Logger logger = Logger.getLogger(GlobalScreen.class.getPackage().getName());
-        logger.setLevel(Level.WARNING);
-
-        try {
-            GlobalScreen.addNativeKeyListener(new Main());
-        } catch (AWTException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void nativeKeyPressed(NativeKeyEvent e) {
-        if (e.getKeyCode() == NativeKeyEvent.VC_T) { // t key
-            if (slash_flag == 0 && t_flag == 0) {
-                robot.keyPress(KeyEvent.VK_HALF_WIDTH); // Press the half-size / em keys
-                robot.keyRelease(KeyEvent.VK_HALF_WIDTH); // Releasing the half-size / em keys
-                t_flag = 1;
-            }
-        }
-
-        if (e.getKeyCode() == NativeKeyEvent.VC_ENTER) { // Enter key
-            if (t_flag == 1 && ent_flag == 1) {
-                Robot robot = null;
-                try {
-                    robot = new Robot();
-                } catch (AWTException e1) {
-                    e1.printStackTrace();
-                }
-                robot.keyPress(KeyEvent.VK_HALF_WIDTH); // Press the half-size / em keys
-                robot.keyRelease(KeyEvent.VK_HALF_WIDTH); // Releasing the half-size / em keys
-                t_flag = 0;
-                ent_flag = 0;
-            } else if (slash_flag == 1) {
-                slash_flag = 0;
-            } else {
-                ent_flag = 1;
-            }
-        } else {
-            ent_flag = 0;
-        }
-
-        if (e.getKeyCode() == NativeKeyEvent.VC_SLASH) { // slash key
-            slash_flag = 1;
-        }
-    }
-
-    public void nativeKeyReleased(NativeKeyEvent e) {
-    }
-
-    public void nativeKeyTyped(NativeKeyEvent e) {
-    }
+	public static void main(String[] args) {
+		try {
+			Main main = new Main();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
